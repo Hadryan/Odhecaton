55bcd2ce6fc155f7633c56200176f2f5ff6939bb
Larry Wright <git@larrypdx.com>
2015-08-10 10:36:04 -0700

    added test task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,31 @@
 var gulp = require('gulp'),
+    args = require('yargs').argv,
     useref = require('gulp-useref'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css'),
     templateCache = require('gulp-angular-templatecache'),
     inject = require('gulp-inject'),
+    minifyHtml = require('gulp-minify-html'),
     del = require('del');
 
 gulp.task('clean', function() {
-    del.sync(['dist']);
+    del.sync(['dist/**/*', 'dist/**/.*']);
 });
 
 gulp.task('html', function () {
     var templates = gulp.src('app/templates/*.html')
+        .pipe(minifyHtml({empty:true}))
         .pipe(templateCache('templates.js', {
             root: 'templates',
             module: 'imslpApp'
-    }));
+        }));
+
     var assets = useref.assets({
         additionalStreams: [templates]
     });
     return gulp.src('app/*.html')
-        .pipe(inject(templates, {relative: true}))
+        .pipe(inject(templates, {relative:true}))
         .pipe(assets)
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCss()))
@@ -39,4 +43,18 @@ gulp.task('build', ['clean', 'html'], function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('test', function(done) {
+    var Server = require('karma').Server;
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: args.singlerun
+    }, done).start();
+});
+
+gulp.task('help', function() {
+    console.log('Usage: gulp <task> [options]');
+    console.log('Tasks: gulp build');
+    console.log('       gulp test [--singlerun]');
+});
+
+gulp.task('default', ['help']);
